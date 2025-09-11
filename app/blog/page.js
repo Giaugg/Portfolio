dd1e6049362574a22626f515f4cc8541613f786d
Giaugg <giaule0111@gmail.com>
2025-09-11 10:45:37 +0700

    fix css and whiteboard

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import "../../styles/blog.css"
 
 export default function BlogPage() {
   return (
---
+++ app/whiteboard/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import "../../styles/cv.css";
+
+export default function CvPage() {
+	return (
+		<div className="whiteboard-layout">
+			{/* Back Button */}
+			<div className="back-button">
+				<Link href="/">← Back to Home</Link>
+			</div>
+            <div>
+                <img src="/images/whiteboard.svg" alt="whiteboard Image" className="whiteboard-image" 
+                style={{ width: "100%", height: "100%" }}
+                />
+            </div>
+		</div>
+	);
+}
